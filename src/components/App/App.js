be400a7ec9647295f25ef27325f43659a782de1e
Kingsley Victor <javaprodigy56@gmail.com>
2020-04-29 18:56:47 +0100

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class App extends Component {
+  render = () => (
+    <div>
+      <span>Hello Codessey, it's time for our Odyssey.</span>
+    </div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "src/index"),
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      include: path.join(__dirname, "src"),
+      exclude: /node_modules/,
+      use: ["babel-loader"]
+    }]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "build"),
+    port: 7000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src/index.html")
+    })
+  ]
+};
